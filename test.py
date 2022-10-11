caf94adb04af232f55bcafce5a700c00852b3ebb
sarahsr6 <sarahsredden@gmail.com>
2022-10-10 17:41:49 -0700

    update 3
    correct code

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hello")
+if __name__=='__main__':
+	x = "ECE_180_DA_DB"
+	if x == "EE_180_DA_DB":
+		print("You are living in 2017")
+	else:
+		x = x + " - Best Class Ever woooooooooo"
+		print(x)
