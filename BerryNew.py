84c3e1d26ac5c27bf407275142f61db2e44068a0
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-28 12:54:53 -0800

    Update BerryNew.py
    try 2

--- BerryNew.py
+++ BerryNew.py
@@ -387,11 +387,11 @@ while True:
     #if 1:                       #Change to '0' to stop showing the angles from the accelerometer
      #   outputString += "#  ACCX Angle %5.2f ACCY Angle %5.2f  #  " % (AccXangle, AccYangle)
 
-    if 1:                       #Change to '0' to stop  showing the angles from the gyro
-        outputString +="\t# GRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f # " % (gyroXangle,gyroYangle,gyroZangle)
+    #if 1:                       #Change to '0' to stop  showing the angles from the gyro
+        #outputString +="\t# GRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f # " % (gyroXangle,gyroYangle,gyroZangle)
 
-    #if 1:                       #Change to '0' to stop  showing the angles from the complementary filter
-       # outputString +="\t#  CFangleX Angle %5.2f   CFangleY Angle %5.2f  #" % (CFangleX,CFangleY)
+    if 1:                       #Change to '0' to stop  showing the angles from the complementary filter
+        outputString +="\t#  CFangleX Angle %5.2f   CFangleY Angle %5.2f  #" % (CFangleX,CFangleY)
 
     #if 1:                       #Change to '0' to stop  showing the heading
       #  outputString +="\t# HEADING %5.2f  tiltCompensatedHeading %5.2f #" % (heading,tiltCompensatedHeading)
@@ -401,7 +401,7 @@ while True:
 
     print(outputString)
     
-    if((gyroXangle<20) and (gyroYangle>-20)):
+    if((CFangleX<70) and (CFangleX>-20)):
         print("off")
     else:
         print("on")
