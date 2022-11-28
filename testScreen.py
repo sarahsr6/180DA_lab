8b711e54fe3530a60f5bc1ea38d6dadd7bfd3500
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-28 15:05:38 -0800

    Update testScreen.py

--- testScreen.py
+++ testScreen.py
@@ -2,8 +2,8 @@ from subprocess import run
 from time import sleep
 
 for i in range(5):
-  run('xrandr --output HDMI-1 --off', shell=True)
+  run('vcgencmd display_power 0 7', shell=True)
   sleep(1)
-  run('xrandr --output HDMI-1 --auto', shell=True)
- # run('vcgencmd display_power 1', shell=True)
+  #run('xrandr --output HDMI-1 --auto', shell=True)
+  run('vcgencmd display_power 1', shell=True)
   sleep(1)
