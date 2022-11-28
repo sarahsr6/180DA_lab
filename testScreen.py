51c0b8f0611fa313687fe02b97914f79c8bb9220
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-28 14:23:24 -0800

    Create testScreen.py
    try 1

---
+++ testScreen.py
@@ -0,0 +1,8 @@
+from subprocess import run
+from time import sleep
+
+for i in range(5):
+  run('vcgencmd display_power 0', shell=True)
+  sleep(1)
+  run('vcgencmd display_power 1', shell=True)
+  sleep(1)
