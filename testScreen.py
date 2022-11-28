b679e8080abfe0cc2fe5524fd8a0a6d3e158a44f
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-28 14:38:45 -0800

    Update testScreen.py

--- testScreen.py
+++ testScreen.py
@@ -2,7 +2,7 @@ from subprocess import run
 from time import sleep
 
 for i in range(5):
-  run('vcgencmd display_power 0')
+  run('vcgencmd display_power 0', shell=True)
   sleep(1)
-  run('vcgencmd display_power 1')
+  run('vcgencmd display_power 1', shell=True)
   sleep(1)
