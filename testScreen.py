781073f545d2a4e4c1bf41fc0d6a087ad6d01aa2
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-28 14:37:14 -0800

    Update testScreen.py

--- testScreen.py
+++ testScreen.py
@@ -2,7 +2,7 @@ from subprocess import run
 from time import sleep
 
 for i in range(5):
-  run('tvservice --off', shell=False)
+  run('vcgencmd display_power 0', shell=False)
   sleep(1)
   run('vcgencmd display_power 1', shell=True)
   sleep(1)
