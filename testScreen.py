51d06aed7e7eb5674589c4e31d2ef01afd9df396
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-29 15:52:02 -0800

    Update testScreen.py

--- testScreen.py
+++ testScreen.py
@@ -3,8 +3,4 @@ from time import sleep
 
 for i in range(10):
   print("hellooooooo test")
-  run('vcgencmd display_power 0', shell=True)
-  sleep(1)
-  #run('xrandr --output HDMI-1 --auto', shell=True)
-  run('vcgencmd display_power 1', shell=True)
   sleep(1)
