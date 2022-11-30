14fb5781019a40d7d8613e62fd90915f6c300289
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-29 16:23:03 -0800

    Update testMultipleSys.py

--- testMultipleSys.py
+++ testMultipleSys.py
@@ -4,6 +4,6 @@ from time import sleep
 y=(0.1)
 #subprocess.run(["python", 'BerryNew.py'])
 #sleep(y)
-subprocess.run(["python", 'testScreen.py'])
+subprocess.run(["python", 'testScreen.py']) & subprocess.run(["python", 'BerryNew.py'])
 
  
