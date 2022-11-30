2bb73762d47f81c24188023d22054b1efab5051c
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-29 18:11:21 -0800

    Update testMultipleSys.py

--- testMultipleSys.py
+++ testMultipleSys.py
@@ -1,14 +1,14 @@
-import subprocess
-from time import sleep
-
-y=(0.1)
-subprocess.Popen(["python", 'testScreen.py'])
-sleep(y)
-subprocess.Popen(["python", 'test.py'])
+#import subprocess
+#from time import sleep
 
 #y=(0.1)
-#subprocess.run(["python", 'BerryNew.py'])
+#subprocess.Popen(["python", 'testScreen.py'])
 #sleep(y)
-#subprocess.run(["python", 'testScreen.py']) & subprocess.run(["python", 'BerryNew.py'])
+#subprocess.Popen(["python", 'test.py'])
+
+import os
+
+os.system("python testScreen.py &")
+os.system("python test.py &")
 
  
