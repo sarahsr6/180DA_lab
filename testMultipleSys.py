38b5805c3ee1c1ef70f78d8e816fd19c17814c30
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2022-11-29 21:52:33 -0800

    Update testMultipleSys.py

--- testMultipleSys.py
+++ testMultipleSys.py
@@ -2,19 +2,21 @@
 #from time import sleep
 
 #y=(0.1)
-#subprocess.Popen(["python", 'testScreen.py'])
+#subprocess.Popen(["python", 'BerryNew.py'])
 #sleep(y)
 #subprocess.Popen(["python", 'test.py'])
 
-#import os
 
-#os.system("python testScreen.py &")
-#os.system("python test.py &")
+import os
+os.system("python BerryNew.py &")
+os.system("python test.py &")
 
-import berryIMUfunction
-import subprocess
+
+
+#import berryIMUfunction
+#import subprocess
  
-if(berryIMUfunction.power() == 0):
-    subprocess.run('vcgencmd display_power 0', shell=True)
-else:
-    subprocess.run('vcgencmd display_power 1', shell=True)
+#if(berryIMUfunction.power() == 0):
+ #   subprocess.run('vcgencmd display_power 0', shell=True)
+#else:
+ #   subprocess.run('vcgencmd display_power 1', shell=True)
