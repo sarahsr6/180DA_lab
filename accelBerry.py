14278af15c90e924a6fe5465986ebd0211895fb1
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2023-02-04 19:46:07 -0800

    Update accelBerry.py

--- accelBerry.py
+++ accelBerry.py
@@ -385,14 +385,14 @@ while True:
     ##################### END Tilt Compensation ########################
 
 
-    if 1:                       #Change to '0' to stop showing the angles from the accelerometer
-        outputString += "#  ACCX Angle %5.2f ACCY Angle %5.2f  #  " % (AccXangle, AccYangle)
+    #if 1:                       #Change to '0' to stop showing the angles from the accelerometer
+    #    outputString += "#  ACCX Angle %5.2f ACCY Angle %5.2f  #  " % (AccXangle, AccYangle)
 
     #if 1:                       #Change to '0' to stop  showing the angles from the gyro
         #outputString +="\t# GRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f # " % (gyroXangle,gyroYangle,gyroZangle)
 
- #   if 1:                       #Change to '0' to stop  showing the angles from the complementary filter
-  #      outputString +="\t#  CFangleX Angle %5.2f   CFangleY Angle %5.2f  #" % (CFangleX,CFangleY)
+    if 1:                       #Change to '0' to stop  showing the angles from the complementary filter
+        outputString +="\t#  CFangleX Angle %5.2f   CFangleY Angle %5.2f  #" % (CFangleX,CFangleY)
 
     #if 1:                       #Change to '0' to stop  showing the heading
       #  outputString +="\t# HEADING %5.2f  tiltCompensatedHeading %5.2f #" % (heading,tiltCompensatedHeading)
