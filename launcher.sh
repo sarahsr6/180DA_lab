4267f50460a0ac1fb8e46bfd60577237f24f09ef
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2023-02-21 17:53:10 -0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/newnewproj/180DA_lab
+cd home/pi/mirror/180DA_lab
 sudo python3 imuPublisher.py
 cd /
